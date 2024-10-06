0c68202d04b4abfc8397d33d84e4a3fe204d50b0
Jaeuk Jeong <jaeuk@jeuke.com>
2024-10-06 12:58:28 +0900

    Fix Error Delete Farm

--- routes/farms.js
+++ routes/farms.js
@@ -164,7 +164,7 @@ router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Farm.findByIdAndDelete(req.params.id)
+    Farm.findById(req.params.id)
       .then((result) => {
         console.log(print);
         if (!result.deletedCount)
